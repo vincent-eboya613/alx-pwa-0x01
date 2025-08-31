eabb3815c8b0d67a9d177fa552f02b051a38a44c
vincent-eboya613 <vincentkidavi@gmail.com>
2025-08-31 16:23:21 +0300

    Create Footer.tsx

---
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -0,0 +1,80 @@
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className='bg-[#171D22] text-white py-10 px-6 md:px-10 lg:px-20'>
+      <div className='flex flex-col md:flex-row justify-between items-center w-full'>
+        {/* Footer Logo */}
+        <h2 className='text-xl md:text-4xl font-semibold mb-4 md:mb-0'>
+          Cine<span className='text-[#E2D609]'>Seek</span>
+        </h2>
+
+        <nav className='flex-1 flex justify-center space-x-6 mb-4 md:mb-0'>
+          <Link
+            href='/'
+            className='hover:text-[#E2D609] text-lg transition-colors duration-300'
+          >
+            Home
+          </Link>
+          <Link
+            href='/movies'
+            className='hover:text-[#E2D609] text-lg transition-colors duration-300'
+          >
+            Movies
+          </Link>
+          <Link
+            href='/contact'
+            className='hover:text-[#E2D609] text-lg transition-colors duration-300'
+          >
+            Contact
+          </Link>
+          <Link
+            href='/privacy'
+            className='hover:text-[#E2D609] text-lg transition-colors duration-300'
+          >
+            Privacy Policy
+          </Link>
+        </nav>
+
+        <div className='flex space-x-4'>
+          <Link
+            href='https://twitter.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-[#E2D609]'
+          >
+            <FontAwesomeIcon icon={faTwitter} size='lg' />
+          </Link>
+          <Link
+            href='https://facebook.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-[#E2D609]'
+          >
+            <FontAwesomeIcon icon={faFacebook} size='lg' />
+          </Link>
+          <Link
+            href='https://instagram.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-[#E2D609]'
+          >
+            <FontAwesomeIcon icon={faInstagram} size='lg' />
+          </Link>
+        </div>
+      </div>
+
+      <div className='mt-8 text-center text-sm text-gray-400'>
+        <p>&copy; 2024 CineSeek. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
